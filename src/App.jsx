0c575a5ce3b64c081b4ba9962e1e00f970308f4a
Busari Moruf <busarimoruf20@gmail.com>
2023-10-23 23:22:29 +0100

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,101 @@
-//  ************** Learning phase 3 ********
+//  ************** Learning phase 5 ********
+// practice phase
+// import { useState } from "react";
+
+// function Counter () {
+//   const [count, setCount] = useState(10)
+  
+//   function addOne () {
+//     setCount(count + 1)
+//   }
+//   return(
+//     <div className="App">
+//       <button onClick={addOne}>count = {count}</button>
+//     </div>
+//   )
+// }
+
+// function NameList () {
+//   const [list, setList] = useState(["Ade", "Ayo", "Adigun", "Abbey"])
+//   const [name, setName] = useState("")
+  
+//   function addName () {
+//     setList([...list, name])
+//   }
+//   return(
+//     <div className="App">
+//       <ul>
+//         {list.map((name) => 
+//         <li key={name}>{name}</li>
+//         )}
+//       </ul>
+//       <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+//       <button onClick={addName}>Add name</button>
+//     </div>
+//   )
+// }
+
+// function App () {
+//   return(
+//     <div>
+//     <Counter />
+//     <NameList />
+//     </div>
+//   )
+// }
+
+// export default App
+
+//  ************** Learning phase 4 ********
 // to add an item to array
 // .... continue from 17minutes
-import { useState } from 'react'
+// import { useState } from 'react'
 
-function NameList() {
-  const [list, setList] = useState(["Jack", "Jill", "John"]);
-  const [name, setName] = useState("");
+// function NameList() {
+//   const [list, setList] = useState(["Jack", "Jill", "John"]);
+//   const [name, setName] = useState("");
 
-  const onAddName = () => {
-    setList([...list, name]);
-  }
-  return (
-    <div>
-      <ul>
-        {list.map((name) => (
-          <li key={name}>{name}</li>
-        ))}
-      </ul>
-      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-      <button onClick={onAddName}>Add Name</button>
-    </div>
-  );
-}
+//   const onAddName = () => {
+//     setList([...list, name]);
+//   }
+//   return (
+//     <div>
+//       <ul>
+//         {list.map((name) => (
+//           <li key={name}>{name}</li>
+//         ))}
+//       </ul>
+//       <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+//       <button onClick={onAddName}>Add Name</button>
+//     </div>
+//   );
+// }
 
-function Counter() {
-  const [count, setCount] = useState(10);
+// function Counter() {
+//   const [count, setCount] = useState(10);
 
-  function addOne () {
-    setCount(count + 1);
-  }
-  return (
-    <div className='App'>
-    <button onClick={addOne}>Count = {count}</button>
-  </div>
-  );
-}
+//   function addOne () {
+//     setCount(count + 1);
+//   }
+//   return (
+//     <div className='App'>
+//     <button onClick={addOne}>Count = {count}</button>
+//   </div>
+//   );
+// }
 
-function App() {
-    return(
-        <div>
-          <Counter />
-          <NameList />
-        </div>
-    );
-}
+// function App() {
+//     return(
+//         <div>
+//           <Counter />
+//           <NameList />
+//         </div>
+//     );
+// }
   
-export default App
+// export default App
 
-//  ************** Learning phase 2 ********
+//  ************** Learning phase 3 ********
 /*
 How to manage a simple scaler using useState
 (how to manage an array)
