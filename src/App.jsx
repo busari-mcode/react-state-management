a3da6b8064a1ca9e0007433f7603af6b71e510d7
Busari Moruf <busarimoruf20@gmail.com>
2023-10-26 23:57:04 +0100

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,51 @@
+//  ************** Learning phase 6 ********
+
+import { useState } from 'react'
+
+function NameList() {
+  const [list, setList] = useState(["Jack", "Jill", "John"]);
+  const [name, setName] = useState("");
+
+  const onAddName = () => {
+    setList([...list, name]);
+  }
+  return (
+    <div>
+      <ul>
+        {list.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={onAddName}>Add Name</button>
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(10);
+
+  function addOne () {
+    setCount(count + 1);
+  }
+  return (
+    <div className='App'>
+    <button onClick={addOne}>Count = {count}</button>
+  </div>
+  );
+}
+
+function App() {
+    return(
+        <div>
+          <Counter />
+          <NameList />
+        </div>
+    );
+}
+  
+export default App
+
 //  ************** Learning phase 5 ********
 // practice phase
 // import { useState } from "react";
@@ -48,7 +96,6 @@
 
 //  ************** Learning phase 4 ********
 // to add an item to array
-// .... continue from 17minutes
 // import { useState } from 'react'
 
 // function NameList() {
